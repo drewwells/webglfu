f2fff15605e2ef5c91b9ffd79e69fd1098eae3f7
Drew Wells <drew.wells00@gmail.com>
2011-09-01 01:05:23 -0400

    Ladies and Gents we have a bezier.  The calculated control points are incorrect

--- graph.js
+++ graph.js
@@ -11,23 +11,28 @@ var arr = [
     10, 8
 ];
 
-arr = (function( seed, length, max ){
-    var arr = [];
-    max = max || 100;
-    for( var i = seed; i < length; i++ ){
-        arr.push( i, Math.floor( Math.random() * 100 ) );
-    }
-    return arr;
-})( 0, 70 );
+var $ = function( id ){
+    return document.getElementById.apply( document, arguments );
+};
+
+
+// arr = (function( seed, length, max ){
+//     var arr = [];
+//     max = max || 100;
+//     for( var i = seed; i < length; i++ ){
+//         arr.push( i, Math.floor( Math.random() * 100 ) );
+//     }
+//     return arr;
+// })( 0, 70 );
+
+arr = [0, 95, 1, 61, 2, 91, 3, 12, 4, 87, 5, 0, 6, 89, 7, 34, 8, 11, 9, 7, 10, 41, 11, 66, 12, 29, 13, 57, 14, 97, 15, 54, 16, 71, 17, 65, 18, 35, 19, 78, 20, 6, 21, 44, 22, 84, 23, 9, 24, 26, 25, 0, 26, 1, 27, 24, 28, 58, 29, 46, 30, 96, 31, 73, 32, 90, 33, 92, 34, 74, 35, 60, 36, 3, 37, 57, 38, 40, 39, 83, 40, 78, 41, 64, 42, 65, 43, 29, 44, 59, 45, 53, 46, 69, 47, 29, 48, 41, 49, 92, 50, 72, 51, 6, 52, 57, 53, 9, 54, 41, 55, 20, 56, 51, 57, 55, 58, 89, 59, 67, 60, 14, 61, 36, 62, 35, 63, 45, 64, 2, 65, 69, 66, 81, 67, 11, 68, 35, 69, 94];
+
 
 function webGLStart() {
     
     function prepareData( arr ){
         var z = 0, i = 0, cleanArr = [];
         for( i = 0; i < arr.length; i = i + 1 ){
-            if( i === 0 ){
-                cleanArr.push( arr[i], 0, 0 );
-            }
             if( i && i % 2 === 0 ){
                 cleanArr.push( z );
             }
@@ -35,19 +40,91 @@ function webGLStart() {
             cleanArr.push ( arr[i] );
         }
         cleanArr.push( z );
-        cleanArr.push( arr[ arr.length - 2 ], 0, 0 );
 
         return injectPoints( cleanArr );
+    }
+
+    function bezier( a, b, c, d, t ){
+        var a3 = a * 3, b3 = b * 3, c3 = c * 3;
+        var ret = a + t*(b3 - a3 + t*(a3-2*b3+c3 + t*(b3-a-c3+d)));
+        return ret;
+    }
+    function bezier3( a, b, c, d, t, dest ){
+        if( dest == null ) dest = [];
+        var x = bezier(a[0], b[0], c[0], d[0], t);
+        dest.push( x );
+        dest.push( 0 );
+        dest.push( 0 ); //sigh hard coded z axis
+        dest.push( x );
+        dest.push( bezier(a[1], b[1], c[1], d[1], t) );
+        //dest.push( bezier(a[2], b[2], c[2], d[2], t) ); skipping z for now
+        dest.push( 0 );
+        return dest;
+    }
+    var ap = Array.prototype.push;
+    function faux( a, b, enda, endb ){
+        //Make control points, okay?
+        var ret = [],
+            cp1 = [ ],
+            cp2 = [ ],
+            dx = 0.25 * ( b[0] - a[0] ),
+            dy = 0.25 * ( b[1] - a[1] );
+
+        //endpoints controlpoint dy
+        if( enda ){
+            cp1.push( a[0] );
+            cp1.push( b[1] > a[1] ? b[1] - dy : a[1] + dy );
+            cp1.push( a[2] );//only true in flat
+        } else {
+            cp1.push( b[0] > a[0] ? b[0] - dx : a[0] + dx );
+            cp1.push( a[1] );
+            cp1.push( a[2] );//only true in flat
+        }
+
+        if( endb ){
+            cp2.push( a[0] );
+            cp2.push( b[1] > a[1] ? b[1] - dy : a[1] + dy );
+            cp2.push( a[2] );//only true in flat
+        } else {
+            cp2.push( b[0] > a[0] ? b[0] - dx : a[0] + dx );
+            cp2.push( a[1] );
+            cp2.push( a[2] );//only true in flat
+        }
+
+        for( var i = 1, count = 500; i < count; i++ ){
+            var t = i / ( count - 1 );
+            ap.apply( ret, bezier3( 
+                a, 
+                cp1,
+                cp2,
+                b,
+                t ) );
+        }
+        return ret;
     }
 
     //Inject points so every point is ( start, pt, end )
     function injectPoints( arr ){
 
-        var ret = [];
-
+        var ret = [], prev, current;
+        //arr = bezier( arr );        
         for( var i = 0, l = arr.length / 3; i < l; i = i + 1 ){
+            current = [ arr[ i*3 ], arr[ i*3 + 1 ], arr[ i*3 + 2 ] ];
+            if( i > 0 ){
+                ap.apply( ret, faux( prev, current, i === 1, i === l  ) );
+            }
             ret.push( arr[ i*3 ], 0, arr[ i*3 + 2 ],
                     arr[ i*3 ], arr[ i*3 + 1 ], arr[ i*3 + 2 ]);
+            prev = current;
+        }
+        return ret;
+    }
+
+    function prepareIndices( iindices ){
+        var ret = [];
+        for( var i = 0; i < iindices; i = i + 4 ){
+            ret.push( i, i + 1, i + 3 );
+            ret.push( i + 1, i + 2, i + 3 );
         }
 
         return ret;
@@ -55,12 +132,12 @@ function webGLStart() {
     
     var matrix = prepareData( arr ),
         iindices = Math.floor( matrix.length / 3 ),
-        indices = [];//prepareIndices( matrix );
+        indices = prepareIndices( iindices );
 
     var graph = new PhiloGL.O3D.Model({
         vertices: matrix,
         colors: [ 1, 0, 0, 1 ],
-        indices: []//indices
+        indices: indices
 
     });
 
@@ -78,7 +155,9 @@ function webGLStart() {
             var gl = app.gl,
                 canvas = app.canvas,
                 program = app.program,
-                camera = new PhiloGL.Camera( 45, .4/*canvas.width / canvas.height*/, 0.1, 1000 ),//app.camera,
+                camera = new PhiloGL.Camera( 45, 
+                                             .3/*canvas.width / canvas.height*/, 
+                                             0.1, 1000 ),//app.camera,
                 view = new PhiloGL.Mat4(),
                 rTri = 0, rSquare = 0;
 
@@ -110,7 +189,7 @@ function webGLStart() {
                 program.setUniform('uMVMatrix', view);
                 program.setUniform('uPMatrix', camera.projection);
             }
-            console.log( 'Rendered: ', Math.floor( graph.$verticesLength / 3 ) * 3 );
+            $( 'rendered' ).innerHTML = 'Vertice Count: ' + Math.floor( graph.$verticesLength / 3 ) * 3;
             var camx = -33, camy = -80, camz = -200;
             function drawScene(){
 
@@ -118,11 +197,13 @@ function webGLStart() {
                 //Draw triangle
                 //camy = camy - 0.1;
                 //console.log( camx, camy, camz );
+
                 graph.position.set(camx, camy, camz);
                 graph.rotation.set(0, rTri, 0);
                 setupElement( graph );
                 program.setBuffer('graph');
-                gl.drawArrays( gl.TRIANGLE_STRIP, 0, Math.floor( graph.$verticesLength / 3 ));
+                gl.drawArrays( gl.TRIANGLE_STRIP, 0, 
+                               Math.floor( graph.$verticesLength / 3 ) );
                 
                 program.setBuffer('indices', {
                     value: graph.indices,
