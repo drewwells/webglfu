4edbdf71b37d216d32f8f12117baf0d4cb21b1f0
Drew Wells <drew.wells00@gmail.com>
2011-09-01 02:30:09 -0400

    Tada, needs a really low aspect ratio to see curves though

--- graph.js
+++ graph.js
@@ -16,16 +16,16 @@ var $ = function( id ){
 };
 
 
-// arr = (function( seed, length, max ){
-//     var arr = [];
-//     max = max || 100;
-//     for( var i = seed; i < length; i++ ){
-//         arr.push( i, Math.floor( Math.random() * 100 ) );
-//     }
-//     return arr;
-// })( 0, 70 );
+arr = (function( seed, length, max ){
+    var arr = [];
+    max = max || 100;
+    for( var i = seed; i < length; i++ ){
+        arr.push( i, Math.floor( Math.random() * 100 ) );
+    }
+    return arr;
+})( 0, 70 );
 
-arr = [0, 95, 1, 61, 2, 91, 3, 12, 4, 87, 5, 0, 6, 89, 7, 34, 8, 11, 9, 7, 10, 41, 11, 66, 12, 29, 13, 57, 14, 97, 15, 54, 16, 71, 17, 65, 18, 35, 19, 78, 20, 6, 21, 44, 22, 84, 23, 9, 24, 26, 25, 0, 26, 1, 27, 24, 28, 58, 29, 46, 30, 96, 31, 73, 32, 90, 33, 92, 34, 74, 35, 60, 36, 3, 37, 57, 38, 40, 39, 83, 40, 78, 41, 64, 42, 65, 43, 29, 44, 59, 45, 53, 46, 69, 47, 29, 48, 41, 49, 92, 50, 72, 51, 6, 52, 57, 53, 9, 54, 41, 55, 20, 56, 51, 57, 55, 58, 89, 59, 67, 60, 14, 61, 36, 62, 35, 63, 45, 64, 2, 65, 69, 66, 81, 67, 11, 68, 35, 69, 94];
+//arr = [0, 95, 1, 61, 2, 91, 3, 12, 4, 87, 5, 0, 6, 89, 7, 34, 8, 11, 9, 7, 10, 41, 11, 66, 12, 29, 13, 57, 14, 97, 15, 54, 16, 71, 17, 65, 18, 35, 19, 78, 20, 6, 21, 44, 22, 84, 23, 9, 24, 26, 25, 0, 26, 1, 27, 24, 28, 58, 29, 46, 30, 96, 31, 73, 32, 90, 33, 92, 34, 74, 35, 60, 36, 3, 37, 57, 38, 40, 39, 83, 40, 78, 41, 64, 42, 65, 43, 29, 44, 59, 45, 53, 46, 69, 47, 29, 48, 41, 49, 92, 50, 72, 51, 6, 52, 57, 53, 9, 54, 41, 55, 20, 56, 51, 57, 55, 58, 89, 59, 67, 60, 14, 61, 36, 62, 35, 63, 45, 64, 2, 65, 69, 66, 81, 67, 11, 68, 35, 69, 94];
 
 
 function webGLStart() {
@@ -73,22 +73,22 @@ function webGLStart() {
         //endpoints controlpoint dy
         if( enda ){
             cp1.push( a[0] );
-            cp1.push( b[1] > a[1] ? b[1] - dy : a[1] + dy );
+            cp1.push( b[1] > a[1] ? b[1] + dy : b[1] - dy );
             cp1.push( a[2] );//only true in flat
         } else {
-            cp1.push( b[0] > a[0] ? b[0] - dx : a[0] + dx );
+            cp1.push( a[0] + dx );
             cp1.push( a[1] );
             cp1.push( a[2] );//only true in flat
         }
 
         if( endb ){
-            cp2.push( a[0] );
-            cp2.push( b[1] > a[1] ? b[1] - dy : a[1] + dy );
-            cp2.push( a[2] );//only true in flat
+            cp2.push( b[0] );
+            cp2.push( b[1] > a[1] ? b[1] - dy : b[1] + dy );
+            cp2.push( b[2] );//only true in flat
         } else {
-            cp2.push( b[0] > a[0] ? b[0] - dx : a[0] + dx );
-            cp2.push( a[1] );
-            cp2.push( a[2] );//only true in flat
+            cp2.push( b[0] - dx );
+            cp2.push( b[1] );
+            cp2.push( b[2] );//only true in flat
         }
 
         for( var i = 1, count = 500; i < count; i++ ){
@@ -132,20 +132,59 @@ function webGLStart() {
     
     var matrix = prepareData( arr ),
         iindices = Math.floor( matrix.length / 3 ),
-        indices = prepareIndices( iindices );
+        indices = prepareIndices( iindices ),
+        theta = 0;
 
     var graph = new PhiloGL.O3D.Model({
         vertices: matrix,
         colors: [ 1, 0, 0, 1 ],
         indices: indices
 
-    });
+    }), pos;
 
     PhiloGL('canvas', {
         program: {
             from: 'ids',
             vs: 'shader-vs',
             fs: 'shader-fs'
+        },
+        events: {
+            onDragStart: function(e) {
+                this.pos = {
+                    x: e.x,
+                    y: e.y
+                };
+                this.dragging = true;
+            },
+            onDragCancel: function(){
+                this.dragging = false;
+            },
+            onDragMove: function(e) {
+                var z = this.camera.position.z,
+                    sign = Math.abs(z) / z,
+                    pos = this.pos,
+                    x = -(pos.x - e.x) / 100,
+                    y = sign * (pos.y - e.y) / 100,
+                    graph = this.scene.models[0];
+
+                graph.position.x += (pos.x - e.x) * this.camera.aspect / 2;
+                graph.position.y += -(pos.y - e.y) / 2;
+                graph.update();
+
+                pos.x = e.x;
+                pos.y = e.y;
+            },
+            onDragEnd: function(){
+                theta = this.scene.models[0].rotation.y;
+                this.dragging = false;
+            },
+            onMouseWheel: function(e) {
+                e.stop();
+                var camera = this.camera;
+
+                camera.position.z += e.wheel * 100;
+                camera.update();
+            }
         },
         onError: function() {
             console.log( this, arguments );
@@ -155,11 +194,16 @@ function webGLStart() {
             var gl = app.gl,
                 canvas = app.canvas,
                 program = app.program,
-                camera = new PhiloGL.Camera( 45, 
-                                             .3/*canvas.width / canvas.height*/, 
-                                             0.1, 1000 ),//app.camera,
+                scene = app.scene,
+                //camera = app.camera,
                 view = new PhiloGL.Mat4(),
-                rTri = 0, rSquare = 0;
+                rTri = 0, rSquare = 0,
+                camera = new PhiloGL.Camera( 45, 0.1, 10, -1000, {
+                position: {
+                    x: 100, y: -100, z: -500
+                }
+            });
+            app.camera = camera;
 
             gl.viewport(0, 0, canvas.width, canvas.height);
             gl.clearColor( 211/255, 200/255, 184/255, 1);
@@ -190,16 +234,22 @@ function webGLStart() {
                 program.setUniform('uPMatrix', camera.projection);
             }
             $( 'rendered' ).innerHTML = 'Vertice Count: ' + Math.floor( graph.$verticesLength / 3 ) * 3;
-            var camx = -33, camy = -80, camz = -200;
+
+
+            var camy = 0;
+            scene.add( graph );
+                graph.position.set( -50, -80, 0 );
             function drawScene(){
+                //scene.add( graph );
+
 
                 gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
-                //Draw triangle
-                //camy = camy - 0.1;
-                //console.log( camx, camy, camz );
-
-                graph.position.set(camx, camy, camz);
-                graph.rotation.set(0, rTri, 0);
+                // if( !app.dragging ){
+                //     theta += 0.005;
+                //     graph.rotation.set(0, theta, 0);
+                // }
+                graph.update();
+                camera.update();
                 setupElement( graph );
                 program.setBuffer('graph');
                 gl.drawArrays( gl.TRIANGLE_STRIP, 0, 
@@ -211,17 +261,13 @@ function webGLStart() {
                     size: 1
                 });
                 gl.drawElements( gl.TRIANGLES, graph.indices.length, gl.UNSIGNED_SHORT, 0 );
+                //scene.render();
 
             }
 
-            function animate(){
-                //rTri = rTri + 0.05;
-                rSquare = rSquare + 0.1;
-            }
             (function tick(){
 
                 drawScene();
-                animate();
                 setTimeout(tick,1000/24);
             })();
             //drawScene();
